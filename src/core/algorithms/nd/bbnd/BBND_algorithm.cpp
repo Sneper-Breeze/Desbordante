1795c6fc0a83dd5957a96fd16ad0a29d95f67eb8
Maksim <billsifer00@gmail.com>
2024-09-09 08:40:27 +0000

    Add basic Algorithm class

---
+++ src/core/algorithms/nd/bbnd/BBND_algorithm.cpp
@@ -0,0 +1,59 @@
+#include "algorithms/nd/bbnd/BBND_algorithm.h"
+#include <boost/unordered_map.hpp>
+
+#include <chrono>
+#include <cstddef>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include <easylogging++.h>
+
+#include "config/descriptions.h"
+#include "config/equal_nulls/option.h"
+#include "config/indices/option.h"
+#include "config/names.h"
+#include "config/option.h"
+#include "config/option_using.h"
+#include "config/tabular_data/input_table/option.h"
+#include "model/table/column_layout_relation_data.h"
+#include "model/table/typed_column_data.h"
+#include "model/types/builtin.h"
+#include "model/types/type.h"
+#include "algorithms/nd/util/build_initial_graph.h"
+
+
+namespace algos {
+Bbnd::Bbnd() : Algorithm({}) {
+    RegisterOptions();
+    MakeOptionsAvailable({config::kTableOpt.GetName(), config::kEqualNullsOpt.GetName()});
+}
+
+void Bbnd::RegisterOptions() {
+    DESBORDANTE_OPTION_USING;
+
+    RegisterOption(config::kTableOpt(&input_table_));
+    RegisterOption(config::kEqualNullsOpt(&is_null_equal_null_));
+}
+
+void Bbnd::LoadDataInternal(){
+    relation_ =
+            ColumnLayoutRelationData::CreateFrom(*input_table_, is_null_equal_null_);
+    input_table_->Reset();
+    if (relation_->GetColumnData().empty()) {
+        throw std::runtime_error("Got an empty dataset: ND verifying is meaningless.");
+    }
+
+    graph_ = std::make_shared<model::NDGraph>(model::NDGraph(nd::util::BuildInitialGraph(*relation_)));
+}
+
+void Bbnd::MakeExecuteOptsAvailable(){
+    // Add here any specific execution opts to be available in future
+}
+
+unsigned long long Bbnd::ExecuteInternal() {
+    // Alghorithm will be here
+}
+
+} // namespace algos
---
+++ src/core/algorithms/nd/bbnd/BBND_algorithm.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "algorithms/algorithm.h"
+#include "algorithms/nd/nd.h"
+#include "util/primitive_collection.h"
+#include "config/equal_nulls/type.h"
+#include "config/tabular_data/input_table_type.h"
+#include "model/table/column_layout_relation_data.h"
+#include "algorithms/nd/model/nd_graph.h"
+#include "algorithms/nd/model/nd_path.h"
+#include <list>
+#include <vector>
+
+namespace algos {
+
+class Bbnd : public Algorithm {
+private:
+    config::InputTable input_table_;
+    config::EqNullsType is_null_equal_null_;
+    util::PrimitiveCollection<model::ND> nd_collection_;
+    std::shared_ptr<ColumnLayoutRelationData> relation_;
+    std::vector<model::NDPath> queue;
+    std::shared_ptr<model::NDGraph> graph_ = nullptr;
+    
+    void RegisterOptions();
+    void ResetState() override;
+    void RegisterND(model::ND nd_to_register);
+    void MakeExecuteOptsAvailable() override;
+    void LoadDataInternal() override;
+    unsigned long long ExecuteInternal() override;
+
+public:
+    Bbnd();
+    
+    /* Returns the list of discovered NDs */
+    std::list<model::ND> const& NdList() const noexcept {
+        return nd_collection_.AsList();
+    }
+
+    std::list<model::ND>& NdList() noexcept {
+        return nd_collection_.AsList();
+    }
+};
+
+} // namesapce algos
